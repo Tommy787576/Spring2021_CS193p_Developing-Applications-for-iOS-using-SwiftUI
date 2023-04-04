4dee948ecb54a1a73343b900c7bd8d795042cf8d
Tommy787576 <tommy787576@gmail.com>
2023-04-04 11:29:33 +0800

    Add AssignmentI swift project

---
+++ AssignmentI_Memorize/AssignmentI_Memorize/AssignmentI_MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentI_MemorizeApp.swift
+//  AssignmentI_Memorize
+//
+//  Created by 林士平 on 2023/4/4.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentI_MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
